7001e16f75dc15f29d88cc8e8c0a2eec85f31e91
pereira <rafaellimapereira@gmail.com>
2013-04-12 15:09:01 -0300

    Rails app, estrutura inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bet::Application.config.secret_token = '6ff43207df5e3fcc00818872e13e392fdab7d2f420dedbf248e868263b25da34e73552aae00d3a3115db35038a198568dd25ba8530d7a44bcf1a20bd439e4b32'
